2a6dcbb677d4b9dbcf61a1c8e4f8eec5c32c1263
Vincent Knauss <vbk737@gmail.com>
2024-12-01 06:48:16 -0600

    handling app command

--- main.c
+++ main.c
@@ -10,9 +10,9 @@
 
 #define cleanup_return(x) { ret = (x); goto cleanup; }
 
-#define discord_get_gateway_url "https://discord.com/api/v10/gateway"
-
-#define gateway_url_max_size 255
+#define discord_api_url "https://discord.com/api/v10"
+#define discord_get_gateway_url (discord_api_url"/gateway")
+#define gateway_query_params "/?v=10&encoding=json"
 
 // https://discord.com/developers/docs/events/gateway#gateway-intents
 #define gateway_intents (1 << 9) /* GUILD_MESSAGES */
@@ -33,68 +33,115 @@ enum gateway_events
     GATEWAY_REQUEST_SOUNDBOARD_SOUNDS = 31,
 };
 
-struct gateway_url_write_data
+enum gateway_interaction_types
 {
-    char* buffer;
-    int offset;
+    GATEWAY_INTERACTION_PING = 1,
+    GATEWAY_INTERACTION_APPLICATION_COMMAND = 2,
+    GATEWAY_INTERACTION_MESSAGE_COMPONENT = 3,
+    GATEWAY_INTERACTION_APPLICATION_COMMAND_AUTOCOMPLETE = 4,
+    GATEWAY_INTERACTION_MODAL_SUBMIT = 5,
 };
 
-size_t gateway_url_write_callback(char* ptr, size_t size, size_t nmemb, void* userdata)
+enum gateway_interaction_callback_types
 {
-    struct gateway_url_write_data* data = userdata;
-    size_t count = gateway_url_max_size - 1 - data->offset;
-    count = count > nmemb ? nmemb : count;
-    memcpy(data->buffer, ptr, count);
+    GATEWAY_INTERACTION_CALLBACK_PONG = 1,
+    GATEWAY_INTERACTION_CALLBACK_CHANNEL_MESSAGE_WITH_SOURCE = 4,
+    GATEWAY_INTERACTION_CALLBACK_DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5,
+    GATEWAY_INTERACTION_CALLBACK_DEFERRED_UPDATE_MESSAGE = 6,
+    GATEWAY_INTERACTION_CALLBACK_UPDATE_MESSAGE = 7,
+    GATEWAY_INTERACTION_CALLBACK_APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8,
+    GATEWAY_INTERACTION_CALLBACK_MODAL = 9,
+    GATEWAY_INTERACTION_CALLBACK_LAUNCH_ACTIVITY = 12,
+};
+
+enum gateway_app_command_types
+{
+    GATEWAY_APP_CMD_CHAT_INPUT = 1,
+    GATEWAY_APP_CMD_USER = 2,
+    GATEWAY_APP_CMD_MESSAGE = 3,
+    GATEWAY_APP_CMD_PRIMARY_ENTRY_POINT = 4,
+};
+
+enum gateway_app_command_option_types
+{
+    GATEWAY_APP_CMD_OPT_SUB_COMMAND = 1,
+    GATEWAY_APP_CMD_OPT_SUB_COMMAND_GROUP = 2,
+    GATEWAY_APP_CMD_OPT_STRING = 3,
+    GATEWAY_APP_CMD_OPT_INTEGER = 4,
+    GATEWAY_APP_CMD_OPT_BOOLEAN = 5,
+    GATEWAY_APP_CMD_OPT_USER = 6,
+    GATEWAY_APP_CMD_OPT_CHANNEL = 7,
+    GATEWAY_APP_CMD_OPT_ROLE = 8,
+    GATEWAY_APP_CMD_OPT_MENTIONABLE = 9,
+    GATEWAY_APP_CMD_OPT_NUMBER = 10,
+    GATEWAY_APP_CMD_OPT_ATTACHMENT = 11,
+};
+
+struct buffer_write_data
+{
+    char* buffer;
+    size_t size;
+    size_t offset;
+};
+
+size_t buffer_write_callback(char* ptr, size_t size, size_t nmemb, void* userdata)
+{
+    struct buffer_write_data* data = userdata;
+    size_t count = size * nmemb;
+    if (count > data->size - data->offset)
+    {
+        size_t required = data->offset + count;
+        data->size = 2 * data->size > required ? 2 * data->size : required;
+        data->buffer = realloc(data->buffer, data->size);
+    }
+
+    memcpy(data->buffer + data->offset, ptr, count);
     data->offset += count;
-    data->buffer[data->offset] = 0;
     return count;
 }
 
-int get_gateway_url(char* buffer, size_t bufsize, const char* queryparams)
+char* get_gateway_url()
 {
-    int ret = 0;
+    char* ret = NULL;
     CURL* c = NULL;
     cJSON* response_json = NULL;
+    struct buffer_write_data buffer_write_data = { 0 };
 
     c = curl_easy_init();
     if (!c)
     {
         fprintf(stderr, "curl_easy_init failed\n");
-        cleanup_return(1);
+        cleanup_return(NULL);
     }
-
-    char gateway_url_results[gateway_url_max_size + 1];
-    struct gateway_url_write_data gateway_url_write_data = { gateway_url_results, 0 };
 
     curl_easy_setopt(c, CURLOPT_URL, discord_get_gateway_url);
     curl_easy_setopt(c, CURLOPT_HTTPGET, 1);
-    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, gateway_url_write_callback);
-    curl_easy_setopt(c, CURLOPT_WRITEDATA, &gateway_url_write_data);
-    if (curl_easy_perform(c) != CURLE_OK)
+    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, buffer_write_callback);
+    curl_easy_setopt(c, CURLOPT_WRITEDATA, &buffer_write_data);
+
+    CURLcode code = curl_easy_perform(c);
+    if (code != CURLE_OK)
     {
-        fprintf(stderr, "curl_easy_perform failed\n");
-        cleanup_return(1);
+        fprintf(stderr, "curl_easy_perform failed: %s\n", curl_easy_strerror(code));
+        cleanup_return(NULL);
     }
 
-    response_json = cJSON_Parse(gateway_url_results);
+    response_json = cJSON_ParseWithLength(buffer_write_data.buffer, buffer_write_data.offset);
     cJSON* gateway_url_json = cJSON_GetObjectItemCaseSensitive(response_json, "url");
     char* gateway_url = cJSON_GetStringValue(gateway_url_json);
     if (!gateway_url)
     {
         fprintf(stderr, "failed to parse gateway url query results\n");
-        cleanup_return(1);
+        cleanup_return(NULL);
     }
 
-    if (queryparams)
-    {
-        snprintf(buffer, bufsize, "%s/?%s", gateway_url, queryparams);
-    }
-    else
-    {
-        strncpy(buffer, gateway_url, bufsize);
-    }
+    int url_length = strlen(gateway_url);
+    ret = malloc(url_length + sizeof(gateway_query_params));
+    memcpy(ret, gateway_url, url_length);
+    memcpy(ret + url_length, gateway_query_params, sizeof(gateway_query_params));
 
 cleanup:
+    free(buffer_write_data.buffer);
     cJSON_Delete(response_json);
     curl_easy_cleanup(c);
     return ret;
@@ -110,6 +157,7 @@ struct gateway_websocket_data
     char* buffer;
     uint32_t sequence;
     const char* token;
+    const char* app_id;
     char* resume_gateway_url;
     char* session_id;
 };
@@ -191,21 +239,6 @@ cleanup:
     return ret;
 }
 
-int handle_ready(struct gateway_websocket_data* data, const cJSON* event_data_json)
-{
-    if (!(data->session_id = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(event_data_json, "session_id"))))
-    {
-        fprintf(stderr, "failed to parse session_id\n");
-        return 1;
-    }
-    if (!(data->resume_gateway_url = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(event_data_json, "resume_gateway_url"))))
-    {
-        fprintf(stderr, "failed to parse resume_gateway_url\n");
-        return 1;
-    }
-    return 0;
-}
-
 int handle_hello(struct gateway_websocket_data* data, const cJSON* event_data_json)
 {
     int ret = 0;
@@ -236,6 +269,141 @@ int handle_hello(struct gateway_websocket_data* data, const cJSON* event_data_js
     }
 
 cleanup:
+    return ret;
+}
+
+int handle_ready(struct gateway_websocket_data* data, const cJSON* event_data_json)
+{
+    if (!(data->session_id = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(event_data_json, "session_id"))))
+    {
+        fprintf(stderr, "failed to parse session_id\n");
+        return 1;
+    }
+    if (!(data->resume_gateway_url = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(event_data_json, "resume_gateway_url"))))
+    {
+        fprintf(stderr, "failed to parse resume_gateway_url\n");
+        return 1;
+    }
+
+    printf("got session_id: %s, resume_gateway_url: %s\n", data->session_id, data->resume_gateway_url);
+
+    return 0;
+}
+
+int send_interaction_response(const cJSON* response_json, const char* interaction_id, const char* interaction_token)
+{
+    int ret = 0;
+    char* json_string = NULL;
+    char* endpoint_url = NULL;
+    CURL* c = NULL;
+    struct curl_slist* headers = NULL;
+    struct buffer_write_data buffer_write_data;
+
+    if (response_json)
+    {
+        json_string = cJSON_PrintUnformatted(response_json);
+    }
+    if (!json_string)
+    {
+        fprintf(stderr, "failed to serialize interaction response json\n");
+        cleanup_return(1);
+    }
+
+    const char* fmt = discord_api_url"/interactions/%s/%s/callback";
+    int sz = snprintf(NULL, 0, fmt, interaction_id, interaction_token);
+    if (sz < 0)
+    {
+        fprintf(stderr, "failed to format interaction callback endpoint url\n");
+        cleanup_return(1);
+    }
+    endpoint_url = malloc(sz + 1);
+    if (snprintf(endpoint_url, sz + 1, fmt, interaction_id, interaction_token) != sz)
+    {
+        fprintf(stderr, "failed to format interaction callback endpoint url\n");
+        cleanup_return(1);
+    }
+
+    if (!(c = curl_easy_init()))
+    {
+        fprintf(stderr, "curl_easy_init failed\n");
+        cleanup_return(1);
+    }
+
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+    curl_easy_setopt(c, CURLOPT_URL, endpoint_url);
+    curl_easy_setopt(c, CURLOPT_POST, 1);
+    curl_easy_setopt(c, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(c, CURLOPT_POSTFIELDS, json_string);
+    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, buffer_write_callback);
+    curl_easy_setopt(c, CURLOPT_WRITEDATA, &buffer_write_data);
+
+    CURLcode code = curl_easy_perform(c);
+    if (code != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform failed: %s\n", curl_easy_strerror(code));
+        cleanup_return(1);
+    }
+
+    printf("interaction callback response: %.*s\n", (int)buffer_write_data.offset, buffer_write_data.buffer);
+
+cleanup:
+    free(buffer_write_data.buffer);
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(c);
+    free(endpoint_url);
+    free(json_string);
+    return ret;
+}
+
+int handle_interaction_create(struct gateway_websocket_data* data, const cJSON* event_data_json)
+{
+    int ret = 0;
+    cJSON* response_json = NULL;
+
+    cJSON* type_json = cJSON_GetObjectItemCaseSensitive(event_data_json, "type");
+    if (!cJSON_IsNumber(type_json))
+    {
+        fprintf(stderr, "failed to parse interaction type\n");
+        cleanup_return(1);
+    }
+    int type = type_json->valueint;
+
+    const char* interaction_id = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(event_data_json, "id"));
+    if (!interaction_id)
+    {
+        fprintf(stderr, "failed to parse interaction id\n");
+        cleanup_return(1);
+    }
+
+    const char* interaction_token = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(event_data_json, "token"));
+    if (!interaction_token)
+    {
+        fprintf(stderr, "failed to parse interaction token\n");
+        cleanup_return(1);
+    }
+
+    if (type == GATEWAY_INTERACTION_APPLICATION_COMMAND)
+    {
+        cJSON* app_cmd_json = cJSON_GetObjectItemCaseSensitive(event_data_json, "data");
+
+        response_json = cJSON_CreateObject();
+        cJSON_AddNumberToObject(response_json, "type", GATEWAY_INTERACTION_CALLBACK_CHANNEL_MESSAGE_WITH_SOURCE);
+        cJSON* response_message_json = cJSON_AddObjectToObject(response_json, "data");
+        cJSON_AddStringToObject(response_message_json, "content", "hello world");
+    }
+
+    if (response_json)
+    {
+        if (send_interaction_response(response_json, interaction_id, interaction_token))
+        {
+            fprintf(stderr, "failed to send interaction response\n");
+            cleanup_return(1);
+        }
+    }
+
+cleanup:
+    cJSON_Delete(response_json);
     return ret;
 }
 
@@ -282,6 +450,10 @@ int handle_gateway_payload(struct gateway_websocket_data* data)
         if (strcmp(event_name, "READY") == 0)
         {
             ret = handle_ready(data, event_data_json);
+        }
+        else if (strcmp(event_name, "INTERACTION_CREATE") == 0)
+        {
+            ret = handle_interaction_create(data, event_data_json);
         }
         else
         {
@@ -392,15 +564,120 @@ CURL* connect_to_gateway(const char* url)
 
     curl_easy_setopt(c, CURLOPT_URL, url);
     curl_easy_setopt(c, CURLOPT_CONNECT_ONLY, 2);
-    curl_easy_setopt(c, CURLOPT_VERBOSE, 1);
-    if (curl_easy_perform(c) != CURLE_OK)
+    // curl_easy_setopt(c, CURLOPT_VERBOSE, 1);
+
+    CURLcode code = curl_easy_perform(c);
+    if (code != CURLE_OK)
     {
-        fprintf(stderr, "curl_easy_perform failed\n");
+        fprintf(stderr, "curl_easy_perform failed: %s\n", curl_easy_strerror(code));
         curl_easy_cleanup(c);
         return NULL;
     }
 
     return c;
+}
+
+int create_app_command(const char* app_id, const char* token, const cJSON* command_json)
+{
+    int ret = 0;
+    char* endpoint_url = NULL;
+    CURL* c = NULL;
+    struct curl_slist* headers = NULL;
+    char* bot_header = NULL;
+    char* json_string = NULL;
+    struct buffer_write_data buffer_write_data = { 0 };
+
+    const char* fmt = discord_api_url"/applications/%s/commands";
+    int sz = snprintf(NULL, 0, fmt, app_id);
+    if (sz < 0)
+    {
+        fprintf(stderr, "failed to format app commands endpoint\n");
+        cleanup_return(1);
+    }
+    endpoint_url = malloc(sz + 1);
+    if (snprintf(endpoint_url, sz + 1, fmt, app_id) != sz)
+    {
+        fprintf(stderr, "failed to format app commands endpoint\n");
+        cleanup_return(1);
+    }
+
+    c = curl_easy_init();
+    if (!c)
+    {
+        fprintf(stderr, "curl_easy_init failed\n");
+        cleanup_return(1);
+    }
+
+    const char* bot_header_fmt = "Authorization: Bot %s";
+    sz = snprintf(NULL, 0, bot_header_fmt, token);
+    if (sz < 0)
+    {
+        fprintf(stderr, "failed to format header\n");
+        cleanup_return(1);
+    }
+    bot_header = malloc(sz + 1);
+    if (snprintf(bot_header, sz + 1, bot_header_fmt, token) != sz)
+    {
+        fprintf(stderr, "failed to format header\n");
+        cleanup_return(1);
+    }
+
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    headers = curl_slist_append(headers, bot_header);
+
+    if (command_json)
+    {
+        json_string = cJSON_PrintUnformatted(command_json);
+    }
+    if (!json_string)
+    {
+        fprintf(stderr, "failed to serialize app command json\n");
+        cleanup_return(1);
+    }
+
+    curl_easy_setopt(c, CURLOPT_URL, endpoint_url);
+    curl_easy_setopt(c, CURLOPT_POST, 1);
+    curl_easy_setopt(c, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(c, CURLOPT_POSTFIELDS, json_string);
+    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, buffer_write_callback);
+    curl_easy_setopt(c, CURLOPT_WRITEDATA, &buffer_write_data);
+
+    CURLcode code = curl_easy_perform(c);
+    if (code != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform failed: %s\n", curl_easy_strerror(code));
+        cleanup_return(1);
+    }
+
+    printf("got response to add app command: %.*s\n", (int)buffer_write_data.offset, buffer_write_data.buffer);
+
+cleanup:
+    free(buffer_write_data.buffer);
+    free(json_string);
+    free(bot_header);
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(c);
+    free(endpoint_url);
+    return ret;
+}
+
+int create_app_commands(struct gateway_websocket_data* data)
+{
+    int ret = 0;
+    cJSON* command_json = cJSON_CreateObject();
+    cJSON_AddStringToObject(command_json, "name", "testcmd");
+    cJSON_AddNumberToObject(command_json, "type", GATEWAY_APP_CMD_CHAT_INPUT);
+    cJSON_AddStringToObject(command_json, "description", "testcmd desc");
+
+    if (create_app_command(data->app_id, data->token, command_json))
+    {
+        fprintf(stderr, "failed to create testcmd\n");
+        cleanup_return(1);
+    }
+
+cleanup:
+    cJSON_Delete(command_json);
+    return ret;
 }
 
 int run_websocket_client(CURL* c)
@@ -413,6 +690,18 @@ int run_websocket_client(CURL* c)
     if (!(gateway_websocket_data.token = getenv("TOKEN")))
     {
         fprintf(stderr, "TOKEN environment variable not set\n");
+        cleanup_return(1);
+    }
+
+    if (!(gateway_websocket_data.app_id = getenv("APP_ID")))
+    {
+        fprintf(stderr, "APP_ID environment variable not set\n");
+        cleanup_return(1);
+    }
+
+    if (create_app_commands(&gateway_websocket_data))
+    {
+        fprintf(stderr, "failed to create app commands\n");
         cleanup_return(1);
     }
 
@@ -479,7 +768,6 @@ int run_websocket_client(CURL* c)
                         cleanup_return(1);
                     }
                     printf("heartbeat_timer: %lu\n", timeouts);
-                    // TODO: send gateway heartbeat
 
                     if (send_heartbeat(&gateway_websocket_data))
                     {
@@ -527,6 +815,7 @@ cleanup:
 int main(int argc, char** argv)
 {
     int ret = 0;
+    char* gateway_url = NULL;
     CURL* c = NULL;
 
     if (curl_global_init(CURL_GLOBAL_DEFAULT) != CURLE_OK)
@@ -535,8 +824,7 @@ int main(int argc, char** argv)
         cleanup_return(1);
     }
 
-    char gateway_url[gateway_url_max_size + 1];
-    if (get_gateway_url(gateway_url, sizeof(gateway_url), "v=10&encoding=json"))
+    if (!(gateway_url = get_gateway_url()))
     {
         fprintf(stderr, "failed to get gateway url\n");
         cleanup_return(1);
@@ -561,6 +849,7 @@ int main(int argc, char** argv)
 
 cleanup:
     curl_easy_cleanup(c);
+    free(gateway_url);
     curl_global_cleanup();
 
     return ret;
